050fad0ad2c08af39d5c46cb7c23353303ca517f
Phquangson <Phquangson2003@gmail.com>
2025-06-28 15:00:09 +0700

    [route][web.php]: Update loginController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => '/'], function () {
+    Route::get('/', [LoginController::class, 'index']);
+});
+
+Route::group(['prefix' => '/'], function () {
+    Route::get('/', [LoginController::class, 'index']);
+    Route::get('/index', [LoginController::class, 'index']);
+});
 
 Route::group(['prefix' => 'register'], function () {
     Route::get('/index', [RegisterController::class, 'index']);
